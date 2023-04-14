7b82cb2eb2f08219c8c7b7e3f446969c08ac8e5d
Carlos A. Michelén Ströfer <c.michelen.strofer@gmail.com>
2023-04-14 11:58:33 -0600

    move JuliaOceanWave development to the SandiaLabs organization.

---
+++ packages/NDBC.jl/ndbc_pluto.jl
@@ -0,0 +1,473 @@
+### A Pluto.jl notebook ###
+# v0.19.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0b630718-c1fe-11ec-0fdc-99e430584e3e
+let
+	using HTTP
+	using TranscodingStreams, CodecZlib
+	using DelimitedFiles
+	using DataFrames
+	using Dates
+	using Unitful
+	using AxisArrays
+end
+
+# ╔═╡ 3ee077b9-2908-4433-af20-b4cc28504305
+function ingredients(path::String)
+	# this is from the Julia source code (evalfile in base/loading.jl)
+	# but with the modification that it returns the module instead of the last object
+	name = Symbol(basename(path))
+	m = Module(name)
+	Core.eval(m,
+		Expr(:toplevel,
+			 :(eval(x) = $(Expr(:core, :eval))($name, x)),
+			 :(include(x) = $(Expr(:top, :include))($name, x)),
+			 :(include(mapexpr::Function, x) = $(Expr(:top, :include))(mapexpr, $name, x)),
+			 :(include($path))))
+	m
+end;
+
+# ╔═╡ 24feedad-9a59-4b3a-bdf4-30f98889771a
+NDBC = ingredients("ndbc.jl").NDBC
+
+# ╔═╡ 05df6345-42d7-488d-a247-2e955b0b5dae
+buoy = 46050;  # PACWAVE
+
+# ╔═╡ 649f64b7-e242-423b-9ff0-1f568c7bc85d
+year = 2021;
+
+# ╔═╡ 46c3250a-a7ca-4ce0-85c7-182be1123271
+bfile = false;
+
+# ╔═╡ 1111572a-b296-4cd2-bb9c-6d972d5a968a
+md"## Omnidirectional 
+### available data"
+
+# ╔═╡ 26a94f52-cd1c-46ab-85a8-b5442f7bbc16
+swden = NDBC.available_omnidirectional()
+
+# ╔═╡ 83f2060c-cc70-4888-911f-59d7a424af96
+unique(swden[!, [:buoy]])
+
+# ╔═╡ 961b8e24-ecad-4dfa-ae77-e07945984a48
+filter(:year=> y -> y==1999, swden)[!,[:buoy]]
+
+# ╔═╡ 9bb11dd3-23b9-4008-8c33-e86cf4f08de9
+pw_swden = NDBC.available_omnidirectional(buoy)
+
+# ╔═╡ 80955691-338e-40a1-aa8d-6911d8d250a8
+typeof(pw_swden)
+
+# ╔═╡ e0eba397-f8e0-4994-811e-1ecab58d5fcf
+md"### request data"
+
+# ╔═╡ 450c784c-fb18-412a-96c8-12199e856c50
+data_swden = NDBC.request_omnidirectional(buoy, year, bfile)
+
+# ╔═╡ 1b04055b-0826-43f7-921e-0d53e2f362fc
+md"""
+## Wave Spectrum
+### available data
+"""
+
+# ╔═╡ b300df80-d71a-4110-9fa6-d78614f62f1b
+avail = NDBC.available()
+
+# ╔═╡ 09e92eb5-1a7a-4dc4-a77f-944ff15fcfc7
+unique(avail[!, [:buoy]])
+
+# ╔═╡ 10dc6bea-6bbe-494c-b0b7-f1f47cdb149f
+avail_pw = NDBC.available(buoy)
+
+# ╔═╡ 9ca4cc7f-a026-40a9-92f6-25ea47a98863
+data = NDBC.request(buoy, year, bfile)
+
+# ╔═╡ d5feb58c-70ad-49ee-9f20-91a46f729d51
+md"## Read File"
+
+# ╔═╡ 5e3cfc22-2b15-4e71-9414-556faf8a2961
+d1 = NDBC.read("41001i2015.txt.gz")
+
+# ╔═╡ 789de66b-8445-4835-bc7c-5cee9403be2a
+d2 = NDBC.read("41001i2015.txt")
+
+# ╔═╡ 499d0fdb-c755-4392-bb39-17961e65ca20
+d3 = NDBC.read("41001i2015.txt.gz", "swden")
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+AxisArrays = "39de3d68-74b9-583c-8d2d-e117c070f3a9"
+CodecZlib = "944b1d66-785c-5afd-91f1-9de20f533193"
+DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
+DelimitedFiles = "8bb1440f-4735-579b-a4ab-409b98df4dab"
+HTTP = "cd3eb016-35fb-5094-929b-558a96fad6f3"
+TranscodingStreams = "3bb67fe8-82b1-5028-8e26-92a6c54297fa"
+Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d"
+
+[compat]
+AxisArrays = "~0.4.5"
+CodecZlib = "~0.7.0"
+DataFrames = "~1.3.3"
+HTTP = "~0.9.17"
+TranscodingStreams = "~0.9.6"
+Unitful = "~1.11.0"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[deps.AxisArrays]]
+deps = ["Dates", "IntervalSets", "IterTools", "RangeArrays"]
+git-tree-sha1 = "cf6875678085aed97f52bfc493baaebeb6d40bcb"
+uuid = "39de3d68-74b9-583c-8d2d-e117c070f3a9"
+version = "0.4.5"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.CodecZlib]]
+deps = ["TranscodingStreams", "Zlib_jll"]
+git-tree-sha1 = "ded953804d019afa9a3f98981d99b33e3db7b6da"
+uuid = "944b1d66-785c-5afd-91f1-9de20f533193"
+version = "0.7.0"
+
+[[deps.Compat]]
+deps = ["Base64", "Dates", "DelimitedFiles", "Distributed", "InteractiveUtils", "LibGit2", "Libdl", "LinearAlgebra", "Markdown", "Mmap", "Pkg", "Printf", "REPL", "Random", "SHA", "Serialization", "SharedArrays", "Sockets", "SparseArrays", "Statistics", "Test", "UUIDs", "Unicode"]
+git-tree-sha1 = "b153278a25dd42c65abbf4e62344f9d22e59191b"
+uuid = "34da2185-b29b-5c13-b0c7-acf172513d20"
+version = "3.43.0"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+
+[[deps.ConstructionBase]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "f74e9d5388b8620b4cee35d4c5a618dd4dc547f4"
+uuid = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
+version = "1.3.0"
+
+[[deps.Crayons]]
+git-tree-sha1 = "249fe38abf76d48563e2f4556bebd215aa317e15"
+uuid = "a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f"
+version = "4.1.1"
+
+[[deps.DataAPI]]
+git-tree-sha1 = "fb5f5316dd3fd4c5e7c30a24d50643b73e37cd40"
+uuid = "9a962f9c-6df0-11e9-0e5d-c546b8b5ee8a"
+version = "1.10.0"
+
+[[deps.DataFrames]]
+deps = ["Compat", "DataAPI", "Future", "InvertedIndices", "IteratorInterfaceExtensions", "LinearAlgebra", "Markdown", "Missings", "PooledArrays", "PrettyTables", "Printf", "REPL", "Reexport", "SortingAlgorithms", "Statistics", "TableTraits", "Tables", "Unicode"]
+git-tree-sha1 = "6c19003824cbebd804a51211fd3bbd81bf1ecad5"
+uuid = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+version = "1.3.3"
+
+[[deps.DataStructures]]
+deps = ["Compat", "InteractiveUtils", "OrderedCollections"]
+git-tree-sha1 = "3daef5523dd2e769dad2365274f760ff5f282c7d"
+uuid = "864edb3b-99cc-5e75-8d2d-829cb0a9cfe8"
+version = "0.18.11"
+
+[[deps.DataValueInterfaces]]
+git-tree-sha1 = "bfc1187b79289637fa0ef6d4436ebdfe6905cbd6"
+uuid = "e2d170a0-9d28-54be-80f0-106bbe20a464"
+version = "1.0.0"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.DelimitedFiles]]
+deps = ["Mmap"]
+uuid = "8bb1440f-4735-579b-a4ab-409b98df4dab"
+
+[[deps.Distributed]]
+deps = ["Random", "Serialization", "Sockets"]
+uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+
+[[deps.Formatting]]
+deps = ["Printf"]
+git-tree-sha1 = "8339d61043228fdd3eb658d86c926cb282ae72a8"
+uuid = "59287772-0a20-5a39-b81b-1366585eb4c0"
+version = "0.4.2"
+
+[[deps.Future]]
+deps = ["Random"]
+uuid = "9fa8497b-333b-5362-9e8d-4d0656e87820"
+
+[[deps.HTTP]]
+deps = ["Base64", "Dates", "IniFile", "Logging", "MbedTLS", "NetworkOptions", "Sockets", "URIs"]
+git-tree-sha1 = "0fa77022fe4b511826b39c894c90daf5fce3334a"
+uuid = "cd3eb016-35fb-5094-929b-558a96fad6f3"
+version = "0.9.17"
+
+[[deps.IniFile]]
+git-tree-sha1 = "f550e6e32074c939295eb5ea6de31849ac2c9625"
+uuid = "83e8ac13-25f8-5344-8a64-a9f2b223428f"
+version = "0.5.1"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.IntervalSets]]
+deps = ["Dates", "Statistics"]
+git-tree-sha1 = "eb381d885e30ef859068fce929371a8a5d06a914"
+uuid = "8197267c-284f-5f27-9208-e0e47529a953"
+version = "0.6.1"
+
+[[deps.InvertedIndices]]
+git-tree-sha1 = "bee5f1ef5bf65df56bdd2e40447590b272a5471f"
+uuid = "41ab1584-1d38-5bbf-9106-f11c6c58b48f"
+version = "1.1.0"
+
+[[deps.IterTools]]
+git-tree-sha1 = "fa6287a4469f5e048d763df38279ee729fbd44e5"
+uuid = "c8e1da08-722c-5040-9ed9-7db0dc04731e"
+version = "1.4.0"
+
+[[deps.IteratorInterfaceExtensions]]
+git-tree-sha1 = "a3f24677c21f5bbe9d2a714f95dcd58337fb2856"
+uuid = "82899510-4779-5014-852e-03e436cf321d"
+version = "1.0.0"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.MbedTLS]]
+deps = ["Dates", "MbedTLS_jll", "Random", "Sockets"]
+git-tree-sha1 = "1c38e51c3d08ef2278062ebceade0e46cefc96fe"
+uuid = "739be429-bea8-5141-9913-cc70e7f3736d"
+version = "1.0.3"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+
+[[deps.Missings]]
+deps = ["DataAPI"]
+git-tree-sha1 = "bf210ce90b6c9eed32d25dbcae1ebc565df2687f"
+uuid = "e1d29d7a-bbdc-5cf2-9ac0-f12de2c33e28"
+version = "1.0.2"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+
+[[deps.OrderedCollections]]
+git-tree-sha1 = "85f8e6578bf1f9ee0d11e7bb1b1456435479d47c"
+uuid = "bac558e1-5e72-5ebc-8fee-abe8a469f55d"
+version = "1.4.1"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+
+[[deps.PooledArrays]]
+deps = ["DataAPI", "Future"]
+git-tree-sha1 = "28ef6c7ce353f0b35d0df0d5930e0d072c1f5b9b"
+uuid = "2dfb63ee-cc39-5dd5-95bd-886bf059d720"
+version = "1.4.1"
+
+[[deps.PrettyTables]]
+deps = ["Crayons", "Formatting", "Markdown", "Reexport", "Tables"]
+git-tree-sha1 = "dfb54c4e414caa595a1f2ed759b160f5a3ddcba5"
+uuid = "08abe8d2-0d0c-5749-adfa-8a2ac140af0d"
+version = "1.3.1"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.Random]]
+deps = ["SHA", "Serialization"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.RangeArrays]]
+git-tree-sha1 = "b9039e93773ddcfc828f12aadf7115b4b4d225f5"
+uuid = "b3c3ace0-ae52-54e7-9d0b-2c1406fd6b9d"
+version = "0.3.2"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[deps.SharedArrays]]
+deps = ["Distributed", "Mmap", "Random", "Serialization"]
+uuid = "1a1011a3-84de-559e-8e89-a11a2f7dc383"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SortingAlgorithms]]
+deps = ["DataStructures"]
+git-tree-sha1 = "b3363d7460f7d098ca0912c69b082f75625d7508"
+uuid = "a2af1166-a08f-5f64-846c-94a0d3cef48c"
+version = "1.0.1"
+
+[[deps.SparseArrays]]
+deps = ["LinearAlgebra", "Random"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+
+[[deps.TableTraits]]
+deps = ["IteratorInterfaceExtensions"]
+git-tree-sha1 = "c06b2f539df1c6efa794486abfb6ed2022561a39"
+uuid = "3783bdb8-4a98-5b6b-af9a-565f29a5fe9c"
+version = "1.0.1"
+
+[[deps.Tables]]
+deps = ["DataAPI", "DataValueInterfaces", "IteratorInterfaceExtensions", "LinearAlgebra", "OrderedCollections", "TableTraits", "Test"]
+git-tree-sha1 = "5ce79ce186cc678bbb5c5681ca3379d1ddae11a1"
+uuid = "bd369af6-aec1-5ad0-b16a-f7cc5008161c"
+version = "1.7.0"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[deps.TranscodingStreams]]
+deps = ["Random", "Test"]
+git-tree-sha1 = "216b95ea110b5972db65aa90f88d8d89dcb8851c"
+uuid = "3bb67fe8-82b1-5028-8e26-92a6c54297fa"
+version = "0.9.6"
+
+[[deps.URIs]]
+git-tree-sha1 = "97bbe755a53fe859669cd907f2d96aee8d2c1355"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.3.0"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[deps.Unitful]]
+deps = ["ConstructionBase", "Dates", "LinearAlgebra", "Random"]
+git-tree-sha1 = "b649200e887a487468b71821e2644382699f1b0f"
+uuid = "1986cc42-f94f-5a68-af5c-568840ba703d"
+version = "1.11.0"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+"""
+
+# ╔═╡ Cell order:
+# ╟─0b630718-c1fe-11ec-0fdc-99e430584e3e
+# ╟─3ee077b9-2908-4433-af20-b4cc28504305
+# ╠═24feedad-9a59-4b3a-bdf4-30f98889771a
+# ╠═05df6345-42d7-488d-a247-2e955b0b5dae
+# ╠═649f64b7-e242-423b-9ff0-1f568c7bc85d
+# ╠═46c3250a-a7ca-4ce0-85c7-182be1123271
+# ╟─1111572a-b296-4cd2-bb9c-6d972d5a968a
+# ╠═26a94f52-cd1c-46ab-85a8-b5442f7bbc16
+# ╠═83f2060c-cc70-4888-911f-59d7a424af96
+# ╠═961b8e24-ecad-4dfa-ae77-e07945984a48
+# ╠═80955691-338e-40a1-aa8d-6911d8d250a8
+# ╠═9bb11dd3-23b9-4008-8c33-e86cf4f08de9
+# ╟─e0eba397-f8e0-4994-811e-1ecab58d5fcf
+# ╠═450c784c-fb18-412a-96c8-12199e856c50
+# ╟─1b04055b-0826-43f7-921e-0d53e2f362fc
+# ╠═b300df80-d71a-4110-9fa6-d78614f62f1b
+# ╠═09e92eb5-1a7a-4dc4-a77f-944ff15fcfc7
+# ╠═10dc6bea-6bbe-494c-b0b7-f1f47cdb149f
+# ╠═9ca4cc7f-a026-40a9-92f6-25ea47a98863
+# ╟─d5feb58c-70ad-49ee-9f20-91a46f729d51
+# ╠═5e3cfc22-2b15-4e71-9414-556faf8a2961
+# ╠═789de66b-8445-4835-bc7c-5cee9403be2a
+# ╠═499d0fdb-c755-4392-bb39-17961e65ca20
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
---
+++ packages/NDBC.jl/src/NDBC.jl
@@ -0,0 +1,169 @@
+
+module NDBC
+
+using HTTP
+using TranscodingStreams, CodecZlib
+using DelimitedFiles
+using DataFrames
+using Dates
+using Unitful
+using Unitful: Hz, °, m
+using AxisArrays
+
+
+function _available(parameter::AbstractString)
+    # scrape website
+    url = "https://www.ndbc.noaa.gov/data/historical/" * parameter * "/"
+    raw = filter(x -> occursin(".txt.gz", x), split(String(HTTP.get(url).body)))
+    # parse
+    filenames = map(x -> String(split(x, "\"")[2]), raw)
+    buoys = map(x -> x[1:5], filenames)
+    years = map(x -> String(split(x, ".")[1][7:end]), filenames)
+    # create DataFrame
+    data = DataFrame("buoy" => buoys, "year" => years, "b_file" => false)
+    # remove entries with bad file names, currently only "42002w2008_old.txt.gz"
+    regular_file(y::String) = length(y) == 4
+    b_file(y::String) = length(y) == 5 && y[1] == 'b'
+    filter!(:year => y -> regular_file(y) || b_file(y), data)
+    # b-files
+    b_files = filter(:year => b_file, data)
+    for row in eachrow(b_files)
+        ibuoy = row.buoy
+        iyear = row.year[2:end]
+        # TODO: there's probably a more efficient way of doing this:
+        data.b_file = @. ifelse(data.buoy == ibuoy && data.year == iyear, true, data.b_file)
+    end
+    filter!(:year => y -> regular_file(y), data)
+    data[!, :year] = parse.(Int, data[!, :year])
+    sort!(data)
+end
+
+
+function _available(parameter::AbstractString, buoy::Union{AbstractString,Int})
+    data = _available(parameter)
+    _filterbuoy(data, buoy)
+end
+
+
+function _request(parameter::AbstractString, buoy::Union{AbstractString,Int}, year::Int, b_file::Bool=false)
+    # get data
+    sep_dict = Dict("swden" => "w", "swdir" => "d", "swdir2" => "i", "swr1" => "j", "swr2" => "k")
+    sep = b_file ? sep_dict[parameter] * "b" : sep_dict[parameter]
+    filename = string(buoy) * sep * string(year) * ".txt.gz"
+    url = "https://www.ndbc.noaa.gov/data/historical/" * parameter * "/" * filename
+    raw = transcode(GzipDecompressor, HTTP.get(url).body)
+    _read(raw, parameter)
+end
+
+
+function _filterbuoy(data::DataFrame, buoy::Union{AbstractString,Int})
+    filter!(row -> row.buoy == string(buoy), data)
+    select!(data, Not(:buoy))
+end
+
+
+function _read(file::Union{AbstractString,Vector{UInt8}}, parameter::AbstractString)
+    # parse data
+    data, header = DelimitedFiles.readdlm(file, header=true)
+    header[1] = strip(header[1], '#')
+    # datetime
+    ncol_date = header[5] == "mm" ? 5 : 4
+    datevec = string.(Int.(data[:, 1:ncol_date]), pad=2)
+    two_digit_year = length(datevec[1, 1]) == 2
+    fmt = two_digit_year ? "yy" : "yyyy"
+    fmt *= "mmddHH"
+    ncol_date == 5 && (fmt *= "MM")
+    dates = DateTime[]
+    for row in eachrow(datevec)
+        push!(dates, DateTime(join(row), fmt))
+    end
+    # data
+    unit_dict = Dict("swden" => m * m / Hz, "swdir" => °, "swdir2" => °, "swr1" => 1, "swr2" => 1)
+    data = data[:, ncol_date+1:end] * unit_dict[parameter]
+    # frequency
+    frequency = parse.(Float64, header[ncol_date+1:end]) * Hz
+    # AxisArray
+    AxisArray(data; time=dates, frequency=frequency)
+end
+
+
+function read(file::AbstractString, parameter::Union{AbstractString,Nothing}=nothing)
+    param_dict = Dict("w" => "swden", "d"  => "swdir", "i" => "swdir2", "j" => "swr1", "k" => "swr2")
+    isnothing(parameter) && (parameter = param_dict[string(file[6])])
+    file[end-2:end] == ".gz" && (file = transcode(GzipDecompressor, Base.read(file, String)))
+    _read(file, parameter)
+end
+
+
+function available_omnidirectional()
+    _available("swden")
+end
+
+
+function available_omnidirectional(buoy::Union{AbstractString,Int})
+    _available("swden", buoy)
+end
+
+
+function request_omnidirectional(buoy::Union{AbstractString,Int}, year::Int, b_file::Bool=false)
+    _request("swden", buoy, year, b_file)
+end
+
+
+function available()
+    den = _available("swden")
+    dir = _available("swdir")
+    dir2 = _available("swdir2")
+    r1 = _available("swr2")
+    r2 = _available("swr1")
+
+    # buoy-year combinations for which all 5 files exist
+    innerjoin(den, dir, dir2, r1, r2, on=[:buoy, :year, :b_file])
+end
+
+
+function available(buoy::Union{AbstractString,Int})
+    data = available()
+    _filterbuoy(data, buoy)
+end
+
+
+function request(buoy::Union{AbstractString,Int}, year::Int, b_file::Bool=false)
+    buoy = string(buoy)
+    den = _request("swden", buoy, year, b_file)
+    dir = _request("swdir", buoy, year, b_file)
+    dir2 = _request("swdir2", buoy, year, b_file)
+    r1 = _request("swr1", buoy, year, b_file)
+    r2 = _request("swr2", buoy, year, b_file)
+
+    time, frequency = den.axes
+    parameter = AxisArrays.Axis{:parameter}([:den, :dir, :dir2, :r1, :r2])
+    AxisArray(cat(den, dir, dir2, r1, r2; dims=3), time, frequency, parameter)
+end
+
+
+function metadata(buoy::Union{AbstractString,Int})
+    url = "https://www.ndbc.noaa.gov/station_page.php?station=" * string(buoy)
+    # keys = ["Water depth", "Watch circle radius"]
+    # for key ∈ keys
+        # data = filter(x -> occursin(key, x), split(String(HTTP.get(url).body), '\n'))[1]
+        # replace([1], "\t" => "")
+        # remove <*>
+        # split on :
+    end
+end
+# TODO: missing data!
+
+
+# TODO: yearly data
+#   quality control
+#     monotonical
+#     % full year
+#     no large gaps: %, largest gaps, etc.
+#     missing values
+
+# TODO: station location & plot map!
+
+# TODO: output as wave spectra
+
+end
